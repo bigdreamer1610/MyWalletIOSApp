0eb83f98ea84a6abfb715506df764b8a349bd913
Vuong Vu Bac Son <son.vuongvubac@V000112s-Mac-mini.local>
2020-09-22 17:37:30 +0700

    initial commit

---
+++ MyWalletProject/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MyWalletProject
+//
+//  Created by Vuong Vu Bac Son on 9/22/20.
+//  Copyright © 2020 Vuong Vu Bac Son. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Pods/Target Support Files/Pods-MyWalletProject/Pods-MyWalletProject-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_MyWalletProject : NSObject
+@end
+@implementation PodsDummy_Pods_MyWalletProject
+@end
