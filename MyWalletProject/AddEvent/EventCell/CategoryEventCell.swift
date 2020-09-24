1305139a69bd260a6bf9fab15658ce5bdc04fbc0
Van Thanh <thanh.dinhvan@V000069s-Mac-mini.local>
2020-09-24 09:09:13 +0700

    vanthanh

---
+++ MyWalletProject/AddEvent/EventCell/CategoryEventCell.swift
@@ -0,0 +1,18 @@
+//
+//  CategoryEventCell.swift
+//  MyWalletProject
+//
+//  Created by Van Thanh on 9/23/20.
+//  Copyright © 2020 Vuong Vu Bac Son. All rights reserved.
+//
+
+import UIKit
+
+class CategoryEventCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
---
+++ MyWalletProject/AddEvent/EventController/AddEventController.swift
@@ -0,0 +1,30 @@
+//
+//  AddEventController.swift
+//  MyWalletProject
+//
+//  Created by Van Thanh on 9/23/20.
+//  Copyright © 2020 Vuong Vu Bac Son. All rights reserved.
+//
+
+import UIKit
+
+class AddEventController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
