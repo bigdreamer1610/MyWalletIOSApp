d1206a670a0bfff250c933c4448cd5ff5491b3fa
Van Thanh <thanh.dinhvan@V000069s-Mac-mini.local>
2020-09-25 13:58:51 +0700

    vanthanh3

--- MyWalletProject/AddEvent/EventController/EventController.swift
+++ MyWalletProject/AddEvent/EventController/EventController.swift
@@ -7,45 +7,166 @@
 //
 
 import UIKit
+import FirebaseDatabase
 
 class EventController: UIViewController, UITableViewDataSource, UITableViewDelegate{
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        2
-    }
-    
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "EventCell", for: indexPath) as! EventCell
-        
-        return cell
-      
-    }
-    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-        100
-    }
+   
     
     
     @IBOutlet weak var EventTable: UITableView!
-    
-    
+    var ref : DatabaseReference!
+    var idUser = "userid1"
+    var dateThis = ""
+    var arrEvent: [Event] = []{
+        didSet{
+            print("dachi")
+            EventTable.reloadData()
+        }
+    }
+    var arrNameEvent = [String]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        navigationItem.rightBarButtonItem = UIBarButtonItem(image: .add, style: .done, target: self, action: #selector(leftAction))
+        ref = Database.database().reference()
+        dateThis = setDate()
         let nib = UINib(nibName: "EventCell", bundle: nil)
         EventTable.register(nib, forCellReuseIdentifier: "EventCell")
         EventTable.delegate = self
         EventTable.dataSource = self
+        
+        
         // Do any additional setup after loading the view.
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    override func viewWillAppear(_ animated: Bool) {
+        getDataCurrenlyApplying()
+        sg.selectedSegmentIndex = 0
     }
-    */
+    
+    
+    @IBOutlet weak var sg: UISegmentedControl!
+    @IBAction func smMode(_ sender: UISegmentedControl) {
+        switch sg.selectedSegmentIndex {
+        case 0:
+            getDataCurrenlyApplying()
+        case 1:
+            getEventFinished()
+        default:
+            print("chon lai")
+        }
+    }
+    
+    
+   
+    
+    
+    func getDataCurrenlyApplying()  {
+          arrEvent.removeAll()
+              self.ref.child("Account").child(idUser).child("event").observe(.value) { snapshot in
+                           for case let child as DataSnapshot in snapshot.children {
+                               guard let dict = child.value as? [String: Any] else {
+                                   return
+                               }
+                             let dateEnd = dict["dateEnd"] as! String
+                              var check = self.checkDay(dayThis: self.dateThis, dateEnd: dateEnd)
+                              if check {
+                                  let img = dict["category"] as! String
+                                          let nameEvent = dict["name"] as! String
+                                         let dateStars = dict["dateStart"] as! String
+                                         let money = dict["spent"] as! Int
+                                     
+                                         let event1 = Event(name: nameEvent, goal: money, dateStart: dateStars, dateEnd: dateEnd, category: img)
+                                self.arrNameEvent.append(nameEvent)
+                                  self.arrEvent.append(event1)
+                              }
+                              else {
+                                print(" dang dien ra ")
+                            }
+                           }
+                       }
+        
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return arrEvent.count
+       }
+       
+       func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+           let cell = tableView.dequeueReusableCell(withIdentifier: "EventCell", for: indexPath) as! EventCell
+        cell.load(event: arrEvent[indexPath.row])
+           return cell
+         
+       }
+       func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+           100
+       }
+    @objc func leftAction() {
+        let add = UIStoryboard.init(name: "AddEvent", bundle: nil).instantiateViewController(identifier: "AddEvent") as! AddEventController
+               //calendar.dateThis = dayThis
+        add.arrayNameEvent = arrNameEvent
+        arrEvent.removeAll()
+               self.navigationController?.pushViewController(add, animated: true)
+        
+        
+    }
+    func setDate() -> String {
+        let date = Date()
+        let format = DateFormatter()
+        format.dateFormat = "dd/MM/yyyy"
+        let formatDate = format.string(from: date)
+        return formatDate
+    }
+    
+    func getEventFinished()  {
+        arrEvent.removeAll()
+        self.ref.child("Account").child(idUser).child("event").observe(.value) { snapshot in
+                     for case let child as DataSnapshot in snapshot.children {
+                         guard let dict = child.value as? [String: Any] else {
+                             return
+                         }
+                       let dateEnd = dict["dateEnd"] as! String
+                        var check = self.checkDay(dayThis: self.dateThis, dateEnd: dateEnd)
+                        if check == false {
+                            let img = dict["category"] as! String
+                                    let nameEvent = dict["name"] as! String
+                                   
+                                   let dateStars = dict["dateStart"] as! String
+                                   let money = dict["spent"] as! Int
+                               
+                                   let event1 = Event(name: nameEvent, goal: money, dateStart: dateStars, dateEnd: dateEnd, category: img)
+                            self.arrEvent.append(event1)
+                        }
+                        else {
+                            print("da ket thuc")
+                        }
+                     }
+                 }
+    }
+    
+    func checkDay( dayThis: String , dateEnd: String) -> Bool {
+        var checkday1 = false
+       let dateFormat = "dd-MM-yyyy"
+       let dateFormatter = DateFormatter()
+       dateFormatter.dateFormat = dateFormat
+       let startDate = dateFormatter.date(from: dayThis)
+       let endDate = dateFormatter.date(from: dateEnd)
+
+       guard let startDate1 = startDate, let endDate2 = endDate else {
+           fatalError("Date Format does not match ⚠️")
+       }
+
+        if startDate1 <= endDate2 {
+            print(checkday1)
+           checkday1 = true
+       } else if startDate1 > endDate2 {
+            print(checkday1)
+            checkday1 = false
+       }
+        return checkday1
 
 }
+
+}
+
+
+
