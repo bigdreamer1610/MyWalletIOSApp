283c1ef1c1c423078f589bd3b194a7a404ed4d47
Van Thanh <thanh.dinhvan@V000069s-Mac-mini.local>
2020-10-02 16:08:16 +0700

    fomat

--- MyWalletProject/AddEvent/EventController/AddEventController.swift
+++ MyWalletProject/AddEvent/EventController/AddEventController.swift
@@ -24,17 +24,6 @@ class AddEventController: UIViewController {
     var event = Event()
     var string = Int()
     var userID = "userid1"
-       
-    
-   // cũ
-   
-    // moi
-
-    
-    
-    
-    
-    
 
     @IBOutlet weak var btSave: UIButton!
     @IBAction func btSave(_ sender: Any) {
@@ -47,7 +36,7 @@ class AddEventController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-         ref = Database.database().reference()
+        ref = Database.database().reference()
         tableViewController = self.children[0] as! AddEventTableController
         
         if state == 0  {
@@ -58,11 +47,11 @@ class AddEventController: UIViewController {
             
         } else {
             self.title = "Add Event"
-              getNewChildTitle()
+            getNewChildTitle()
         }
         
-       
-       
+        
+        
         
         setUpView()
     }
@@ -75,7 +64,7 @@ class AddEventController: UIViewController {
         let formatDate = format.string(from: date)
         return formatDate
     }
-
+    
     func setUpView()  {
         btSave.layer.cornerRadius = 10
         btSave.layer.masksToBounds = false
@@ -85,83 +74,83 @@ class AddEventController: UIViewController {
     }
     func leftAction(){
         if state == 1 {
-              if tableViewController.tfDate.text! != ""   && (tableViewController.tfNameEvent.text != "") {
-                      
-                      if  arrayNameEvent.contains((tableViewController.tfNameEvent.text)!) == false {
-                          
-                          let event = [ "id": String(newChild),
-                                         "name": (tableViewController.tfNameEvent.text)! ,
-                                        "date": tableViewController.tfDate.text!,
-                                       "eventImage": tableViewController.eventImg,
-                                       "spent": 0]
-                              as [String : Any]
-                          self.event.date = tableViewController.tfDate.text!
-                  self.ref.child("Account").child(userID).child("event").child(String(string)).updateChildValues(event,withCompletionBlock: { error , ref in
-                                     if error == nil {
-                                      self.completionHandler?(self.event)
-                                  self.navigationController?.popViewController(animated: true)
-                                     }else{
-                                     }
-                                 } )
-                      } else {
-                          let alert = UIAlertController(title: "error", message: "Event này đã tồn tại", preferredStyle: UIAlertController.Style.actionSheet)
-                          alert.addAction(UIAlertAction(title: "ok", style: UIAlertAction.Style.default, handler: nil))
-                           self.present(alert, animated: true, completion: nil)
-                      }
-                      
-                      
-                  } else {
-                      let alert = UIAlertController(title: "error", message: "Moi nhap day du", preferredStyle: UIAlertController.Style.actionSheet)
-                                 alert.addAction(UIAlertAction(title: "ok", style: UIAlertAction.Style.default, handler: nil))
-                                  self.present(alert, animated: true, completion: nil)
-                      
-                      }
+            if tableViewController.tfDate.text! != ""   && (tableViewController.tfNameEvent.text != "") {
+                
+                if  arrayNameEvent.contains((tableViewController.tfNameEvent.text)!) == false {
+                    
+                    let event = [ "id": String(newChild),
+                                  "name": (tableViewController.tfNameEvent.text)! ,
+                                  "date": tableViewController.tfDate.text!,
+                                  "eventImage": tableViewController.eventImg,
+                                  "spent": 0]
+                        as [String : Any]
+                    self.event.date = tableViewController.tfDate.text!
+                    self.ref.child("Account").child(userID).child("event").child(String(string)).updateChildValues(event,withCompletionBlock: { error , ref in
+                        if error == nil {
+                            self.completionHandler?(self.event)
+                            self.navigationController?.popViewController(animated: true)
+                        }else{
+                        }
+                    } )
+                } else {
+                    let alert = UIAlertController(title: "error", message: "Event này đã tồn tại", preferredStyle: UIAlertController.Style.actionSheet)
+                    alert.addAction(UIAlertAction(title: "ok", style: UIAlertAction.Style.default, handler: nil))
+                    self.present(alert, animated: true, completion: nil)
+                }
+                
+                
+            } else {
+                let alert = UIAlertController(title: "error", message: "Moi nhap day du", preferredStyle: UIAlertController.Style.actionSheet)
+                alert.addAction(UIAlertAction(title: "ok", style: UIAlertAction.Style.default, handler: nil))
+                self.present(alert, animated: true, completion: nil)
+                
+            }
         } else {
             if tableViewController.tfNameEvent.text != "" {
                 if arrayNameEvent.contains(tableViewController.tfNameEvent.text!) == false {
                     newChild = Int(tableViewController.event.id!)!
                     let event = [ "id": String(newChild),
-                                           "name": (tableViewController.tfNameEvent.text)! ,
-                                          "date": tableViewController.tfDate.text!,
-                                         "eventImage": tableViewController.eventImg,
-                                         "spent": 0]
-                                as [String : Any]
-                            self.event.date = tableViewController.tfDate.text!
+                                  "name": (tableViewController.tfNameEvent.text)! ,
+                                  "date": tableViewController.tfDate.text!,
+                                  "eventImage": tableViewController.eventImg,
+                                  "spent": 0]
+                        as [String : Any]
+                    self.event.date = tableViewController.tfDate.text!
                     self.ref.child("Account").child(userID).child("event").child(String(newChild)).updateChildValues(event,withCompletionBlock: { error , ref in
-                                       if error == nil {
-                                        self.completionHandler?(self.event)
-                                    self.navigationController?.popViewController(animated: true)
-                                       }else{
-                                       }
-                                   } )
+                        if error == nil {
+                            self.completionHandler?(self.event)
+                            self.navigationController?.popViewController(animated: true)
+                        }else{
+                        }
+                    } )
                 }
             }
         }
-       }
-
-    func getNewChildTitle(){
-        self.ref.child("Account").child(userID).child("event").queryLimited(toLast: 1).observeSingleEvent(of: .value, with: {
-                snapshot in
-                for category in snapshot.children{
-                    print(snapshot.childrenCount)
-                    if  snapshot.childrenCount == nil   {
-                        self.string = 0
-                    } else{
-                        self.string = Int((category as AnyObject).key)! + 1
-                        print(self.string)
-                    }
-                   
-                    
-                    
-                }
-                
-            })
-    
     }
     
-
-   
-
+    func getNewChildTitle(){
+        self.ref.child("Account").child(userID).child("event").queryLimited(toLast: 1).observeSingleEvent(of: .value, with: {
+            snapshot in
+            for category in snapshot.children{
+                print(snapshot.childrenCount)
+                if  snapshot.childrenCount == nil   {
+                    self.string = 0
+                } else{
+                    self.string = Int((category as AnyObject).key)! + 1
+                    print(self.string)
+                }
+                
+                
+                
+            }
+            
+        })
+        
+    }
+    
+    
+    
+    
 }
 extension AddEventController:  AddEventTableControllerDelegate{
     func logoutTappped() {
---
+++ MyWalletProject/AddEvent/Format/FormatNumber.swift
@@ -0,0 +1,22 @@
+//
+//  FormatNumber.swift
+//  MyWalletProject
+//
+//  Created by Van Thanh on 10/2/20.
+//  Copyright © 2020 Vuong Vu Bac Son. All rights reserved.
+//
+
+import UIKit
+
+class FormatNumber: NSObject {
+    public func formatInt(so: Int)  -> String{
+           
+           let numberFormatter = NumberFormatter()
+           numberFormatter.numberStyle = .decimal
+           numberFormatter.maximumFractionDigits = 3
+           var format = numberFormatter.string(from: so as! NSNumber)!
+           return format + " Đ"
+       }
+
+
+}
