f6bd3baa54588177d55ee40a08713694aba1fc7c
bacsonvv <son.vuongvubac@vti.com.vn>
2020-10-06 10:08:28 +0700

    update settings data

--- MyWalletProject/UserSettings/Settings/SettingsUseCase.swift
+++ MyWalletProject/UserSettings/Settings/SettingsUseCase.swift
@@ -10,14 +10,14 @@ import Foundation
 import FirebaseDatabase
 
 protocol SettingsUseCaseDelegate {
-    func saveUserInfoToDB(_ user: Account)
+    func responseData(_ user: Account)
 }
 
 class SettingsUseCase {
     var delegate: SettingsUseCaseDelegate?
 }
 
-extension SettingsUseCase: SettingsUseCaseDelegate {
+extension SettingsUseCase {
     func saveUserInfoToDB(_ user: Account) {
         let userInfo = [
             "name": user.name!,
@@ -33,6 +33,30 @@ extension SettingsUseCase: SettingsUseCaseDelegate {
             error, ref in
             if error == nil {}
             else {}
+        })
+    }
+    
+    // MARK: - Dang bi loi khong biet vi sao!!!!!!
+    func getUserInfoFromDB(_ userId: String) {
+        var user: Account = Account()
+        Defined.ref.child("Account").child("userid1").child("information").observe(.value, with: { snapshot in
+            for case let child as DataSnapshot in snapshot.children {
+                guard let dict = child.value as? [String:Any] else {
+                    print("Error")
+                    return
+                }
+                
+                user.address = dict["address"] as? String
+                user.balance = dict["balance"] as? Int
+                user.dateOfBirth = dict["dateOfBirth"] as? String
+                user.email = dict["email"] as? String
+                user.gender = dict["gender"] as? String
+                user.language = dict["language"] as? String
+                user.name = dict["name"] as? String
+                user.phoneNumber = dict["phoneNumber"] as? String
+            }
+            
+            self.delegate?.responseData(user)
         })
     }
 }
