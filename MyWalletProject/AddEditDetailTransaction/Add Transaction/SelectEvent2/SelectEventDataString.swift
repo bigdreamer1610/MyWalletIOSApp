6d3a74cfda8294a3beea9175d1c5b6ef22ad0659
Bac <ngonhatdongho>
2020-10-12 17:50:40 +0700

    chuyen ngon ngu

---
+++ MyWalletProject/AddEditDetailTransaction/Add Transaction/SelectEvent2/SelectEventDataString.swift
@@ -0,0 +1,13 @@
+//
+//  SelectEventDataString.swift
+//  MyWalletProject
+//
+//  Created by BAC Vuong Toan (VTI.Intern) on 10/12/20.
+//  Copyright © 2020 Vuong Vu Bac Son. All rights reserved.
+//
+
+import Foundation
+
+enum SelectEventDataString: String {
+    case event = "Event"
+}
